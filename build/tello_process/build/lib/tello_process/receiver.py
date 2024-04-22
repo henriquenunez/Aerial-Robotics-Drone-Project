3e4ded9bed484cb77a9bcd9ac195dced9fd1d696
DanMS98 <d.moradisabzevar@gmail.com>
2024-04-22 17:43:11 +0300

    added launch to install folder

--- build/tello_process/build/lib/tello_process/receiver.py
+++ build/tello_process/build/lib/tello_process/receiver.py
@@ -2,8 +2,8 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
+from std_msgs.msg import Float32MultiArray
 import cv2
-
 
 class TelloSubscriber(Node):
 
@@ -15,17 +15,21 @@ class TelloSubscriber(Node):
             self.image_callback,
             rclpy.qos.qos_profile_sensor_data
         )
-        self.subscription  
+        self.frame_coord_pub = self.create_publisher(Float32MultiArray, '/frame_coord', 10)
         self.get_logger().info("Image subscriber node initialized")
 
-
-
     def image_callback(self, msg):
-        self.get_logger().info("Received an image")
+        #self.get_logger().info("Received an image")
         cv_image = CvBridge().imgmsg_to_cv2(msg, "bgr8")
         cv2.imshow("Tello Image", cv_image)
         cv2.waitKey(1)
+        self.process_frame(cv_image)
 
+    def process_frame(self, image):
+        # TODO: find the center coordinates for the frontmost frame
+        array = Float32MultiArray(data=[20.0, 20.0, 320.0, 20.0])
+        self.frame_coord_pub.publish(array)
+        #self.get_logger().info("Processed frame")
 
 def main(args=None):
     rclpy.init(args=args)
@@ -36,6 +40,6 @@ def main(args=None):
     tello_sub.destroy_node()
     rclpy.shutdown()
 
-
 if __name__ == '__main__':
     main()
+
