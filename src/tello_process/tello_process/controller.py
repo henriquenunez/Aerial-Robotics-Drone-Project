a2fb2dcfb0621adfa35a2aafcadcc4c6bce86a36
Hiram <henrique.nunez@yahoo.com>
2024-04-23 16:29:28 +0300

    Feat: basic control (bad)

--- src/tello_process/tello_process/controller.py
+++ src/tello_process/tello_process/controller.py
@@ -21,28 +21,55 @@ class TelloController(Node):
             self.frame_coord_cb,
             rclpy.qos.qos_profile_sensor_data
         )
+        self.tello_vel_pub = self.create_publisher(Twist, '/drone1/cmd_vel', 10)
 
-        time.sleep(3)
         self.tello_client = self.create_client(TelloAction, '/drone1/tello_action')
+        time.sleep(3)
         while not self.tello_client.wait_for_service(timeout_sec=2.0):
             self.get_logger().info('Waiting for TelloAction service')
+
         self.tello_req = TelloAction.Request()
         self.tello_req.cmd = 'takeoff'
         takeoff_future = self.tello_client.call_async(self.tello_req)
         rclpy.spin_until_future_complete(self, takeoff_future)
         self.get_logger().info('took off!')
-        self.tello_vel_pub = self.create_publisher(Twist, '/drone1/cmd_vel', 10)
 
     def frame_coord_cb(self, data):
-        #self.get_logger().info(f'Got: {data.data}')
-        twist_msg = Twist()
-        twist_msg.linear.x = random.uniform(-0.5, 0.5)
-        twist_msg.linear.y = random.uniform(-0.5, 0.5)
-        twist_msg.linear.z = random.uniform(-0.5, 0.5)
-        twist_msg.angular.x = random.uniform(-0.5, 0.5)
-        twist_msg.angular.y = random.uniform(-0.5, 0.5)
-        twist_msg.angular.z = random.uniform(-0.5, 0.5)
+        self.get_logger().info(f'Got: {data.data}')
+
+        twist_msg = self.control(data.data)
+        
         self.tello_vel_pub.publish(twist_msg)
+
+    def control(self, frame_target_coord):
+        twist_msg = Twist()
+        tgt_x, tgt_y = frame_target_coord
+
+        speed = 0.01
+        
+        
+        twist_msg.linear.x = 0.0
+        twist_msg.linear.y = 0.0
+        twist_msg.linear.z = 0.0
+        twist_msg.angular.x = 0.0
+        twist_msg.angular.y = 0.0
+        twist_msg.angular.z = 0.0
+
+        twist_msg.linear.x = speed/5
+        
+        if tgt_x > 0.1:
+            twist_msg.linear.z = -speed
+ 
+        if tgt_x < -0.1:
+            twist_msg.linear.z = speed
+ 
+        if tgt_y > 0.1:
+            twist_msg.linear.y = -speed
+ 
+        if tgt_y < -0.1:
+            twist_msg.linear.y = speed
+
+        return twist_msg
 
 def main(args=None):
     rclpy.init(args=args)
